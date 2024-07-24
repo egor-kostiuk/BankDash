91d2d9e48d801c1809a611e3486df8087e8a7b21
Egor Kostiuk <kostukegor729@gmail.com>
2024-07-24 11:11:13 +0300

    fix: starter 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
+import './styles/App.css'
 
-function App() {
+export default function App() {
   return (
     <div>
       <h1>Test</h1>
     </div>
   )
 }
-
-export default App
