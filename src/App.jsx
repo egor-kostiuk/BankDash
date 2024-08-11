225e2c817b8f88fca4d28e08c24ca8026edba980
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-11 14:08:02 +0300

    feat: add profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {useEffect, useState} from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {auth} from "./services/api/firebase.js";
+import {auth} from "/src/services/api/firebase.js";
 
 import {LoginPage} from "/src/app/pages/Auth/LoginPage.jsx";
 import {SignUpPage} from "/src/app/pages/Auth/SignUpPage.jsx";
+import {HomePage} from "/src/app/pages/HomePage.jsx";
 
 import './styles/App.css';
 
@@ -21,6 +22,7 @@ export default function App() {
         <Route exact path={'/'} element={<LoginPage/>} />
         <Route exact path={'/login'} element={<LoginPage/>} />
         <Route exact path={'/sign-up'} element={<SignUpPage/>} />
+        <Route exact path={'/bankdash'} element={<HomePage/>} /> {/* TODO: if user signed up */}
       </Routes>
     </Router>
   )
