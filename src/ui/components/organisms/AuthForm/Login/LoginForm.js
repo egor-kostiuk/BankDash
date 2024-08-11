dee25ffece7b751f432461c190016b38593e7041
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-11 15:37:48 +0300

    fix: login navigate

--- src/ui/components/organisms/AuthForm/Login/LoginForm.js
+++ src/ui/components/organisms/AuthForm/Login/LoginForm.js
@@ -1,9 +1,11 @@
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 import {signInWithEmailAndPassword} from "firebase/auth";
 import {auth} from "/src/services/api/firebase.js";
 
 export const useLogin = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -12,7 +14,7 @@ export const useLogin = () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       console.log("User logged in Successfully");
-      window.location.href = "/bankdash";
+      navigate('/bankdash');
     } catch (error) {
       console.error(error.message);
     }
