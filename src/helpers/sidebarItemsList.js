5bd0895878e807e7e88bddb8d95c6b039051f9a3
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-12 16:40:51 +0300

    update: optimize sidebar

---
+++ src/helpers/sidebarItemsList.js
@@ -0,0 +1,12 @@
+import {sidebarImgList} from "./sidebarImgList.js";
+
+export const sidebarItemsList = [
+  { img: sidebarImgList.dashboard, label: 'Dashboard' },
+  { img: sidebarImgList.transactions, label: 'Transactions' },
+  { img: sidebarImgList.account, label: 'Account' },
+  { img: sidebarImgList.investments, label: 'Investments' },
+  { img: sidebarImgList.creditCards, label: 'Credit-Cards' },
+  { img: sidebarImgList.loans, label: 'Loans' },
+  { img: sidebarImgList.services, label: 'Services' },
+  { img: sidebarImgList.settings, label: 'Settings' },
+]
--- src/ui/components/organisms/Sidebar/Sidebar.jsx
+++ src/ui/components/organisms/Sidebar/Sidebar.jsx
@@ -1,19 +1,21 @@
 import {SidebarButtonBox} from "/src/ui/components/molecules/SidebarButtonBox/SidebarButtonBox.jsx";
-import {sidebarImgList} from "/src/helpers/sidebarList.js";
+import {sidebarItemsList} from "/src/helpers/sidebarItemsList.js";
 
 import "./Sidebar.css";
 
 export const Sidebar = () => {
   return (
-    <aside className={'sidebar'}>
-      <SidebarButtonBox img={sidebarImgList.dashboard} label={'Dashboard'}/>
-      <SidebarButtonBox img={sidebarImgList.transactions} label={'Transactions'}/>
-      <SidebarButtonBox img={sidebarImgList.account} label={'Account'}/>
-      <SidebarButtonBox img={sidebarImgList.investments} label={'Investments'}/>
-      <SidebarButtonBox img={sidebarImgList.creditCards} label={'Credit-Cards'}/>
-      <SidebarButtonBox img={sidebarImgList.loans} label={'Loans'}/>
-      <SidebarButtonBox img={sidebarImgList.services} label={'Services'}/>
-      <SidebarButtonBox img={sidebarImgList.settings} label={'Settings'}/>
+    <aside className='sidebar'>
+      <ul className='sidebar-list'>
+        {sidebarItemsList.map((item, index) => (
+          <SidebarButtonBox
+            key={index}
+            className='sidebar-list-item'
+            img={item.img}
+            label={item.label}
+          />
+        ))}
+      </ul>
     </aside>
   )
 }
