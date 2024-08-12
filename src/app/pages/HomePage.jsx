180df92c86e445b5c5276ad7cfbaf07fb1bf4305
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-12 11:20:56 +0300

    fix: nome page block

--- src/app/pages/HomePage.jsx
+++ src/app/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import {Profile} from "../../ui/components/molecules/Profile/Profile.jsx";
 
 export const HomePage = () => {
   return (
-    <div>
+    <>
       <Profile/>
-    </div>
+    </>
   )
 }
