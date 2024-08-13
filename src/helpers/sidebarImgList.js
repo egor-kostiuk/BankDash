fb4f078ad9bcae149c947e985941cdf394c164ad
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-13 11:53:37 +0300

    fix: use absolute imports

--- src/helpers/sidebarImgList.js
+++ src/helpers/sidebarImgList.js
@@ -1,18 +1,35 @@
+import dashboardImg from '/src/assets/img/sidebar/dashboard.svg';
+import dashboardActiveImg from '/src/assets/img/sidebar/dash-active.svg';
+import transactionsImg from '/src/assets/img/sidebar/transactions.svg';
+import transactionsActiveImg from '/src/assets/img/sidebar/transac-active.svg';
+import accountImg from '/src/assets/img/sidebar/account.svg';
+import accountActiveImg from '/src/assets/img/sidebar/acc-active.svg';
+import investmentsImg from '/src/assets/img/sidebar/investments.svg';
+import investmentsActiveImg from '/src/assets/img/sidebar/invest-active.svg';
+import creditCardsImg from '/src/assets/img/sidebar/credit-cards.svg';
+import creditCardsActiveImg from '/src/assets/img/sidebar/cards-active.svg';
+import loansImg from '/src/assets/img/sidebar/loans.svg';
+import loansActiveImg from '/src/assets/img/sidebar/loa-active.svg';
+import servicesImg from '/src/assets/img/sidebar/services.svg';
+import servicesActiveImg from '/src/assets/img/sidebar/serv-active.svg';
+import settingsImg from '/src/assets/img/sidebar/settings.svg';
+import settingsActiveImg from '/src/assets/img/sidebar/sett-active.svg';
+
 export const sidebarImgList = {
-  dashboard: "/src/assets/img/sidebar/dashboard.svg",
-  dashboardActive: "/src/assets/img/sidebar/dash-active.svg",
-  transactions: "/src/assets/img/sidebar/transactions.svg",
-  transactionsActive: "/src/assets/img/sidebar/transac-active.svg",
-  account: "/src/assets/img/sidebar/account.svg",
-  accountActive: "/src/assets/img/sidebar/acc-active.svg",
-  investments: "/src/assets/img/sidebar/investments.svg",
-  investmentsActive: "/src/assets/img/sidebar/invest-active.svg",
-  creditCards: "/src/assets/img/sidebar/credit-cards.svg",
-  creditCardsActive: "/src/assets/img/sidebar/cards-active.svg",
-  loans: "/src/assets/img/sidebar/loans.svg",
-  loansActive: "/src/assets/img/sidebar/loa-active.svg",
-  services: "/src/assets/img/sidebar/services.svg",
-  servicesActive: "/src/assets/img/sidebar/serv-active.svg",
-  settings: "/src/assets/img/sidebar/settings.svg",
-  settingsActive: "/src/assets/img/sidebar/sett-active.svg",
+  dashboard: dashboardImg,
+  dashboardActive: dashboardActiveImg,
+  transactions: transactionsImg,
+  transactionsActive: transactionsActiveImg,
+  account: accountImg,
+  accountActive: accountActiveImg,
+  investments: investmentsImg,
+  investmentsActive: investmentsActiveImg,
+  creditCards: creditCardsImg,
+  creditCardsActive: creditCardsActiveImg,
+  loans: loansImg,
+  loansActive: loansActiveImg,
+  services: servicesImg,
+  servicesActive: servicesActiveImg,
+  settings: settingsImg,
+  settingsActive: settingsActiveImg,
 }
