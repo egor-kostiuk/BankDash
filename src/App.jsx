2da2c179929cc8df65ed1c63114a2fb393db0dc5
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-13 10:19:10 +0300

    fix: rename home page to dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {auth} from "/src/services/api/firebase.js";
 
 import {LoginPage} from "/src/app/pages/Auth/LoginPage.jsx";
 import {SignUpPage} from "/src/app/pages/Auth/SignUpPage.jsx";
-import {HomePage} from "/src/app/pages/HomePage.jsx";
+import {DashboardPage} from "/src/app/pages/DashboardPage.jsx";
 import {TestPage} from "./app/pages/TestPage.jsx";
 
 import './styles/App.css';
@@ -23,7 +23,7 @@ export default function App() {
         <Route exact path={'/'} element={<LoginPage/>} />
         <Route exact path={'/login'} element={<LoginPage/>} />
         <Route exact path={'/sign-up'} element={<SignUpPage/>} />
-        <Route exact path={'/bankdash'} element={<HomePage/>} /> {/* TODO: if user signed up */}
+        <Route exact path={'/bankdash'} element={<DashboardPage/>} /> {/* TODO: if user signed up */}
         <Route exact path={'/test'} element={<TestPage/>} /> {/* TODO: implement other pages */}
       </Routes>
     </Router>
--- src/app/pages/DashboardPage.jsx
+++ src/app/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import {Main} from "/src/ui/components/templates/Main/Main.jsx";
 import {Header} from "/src/ui/components/templates/Header/Header.jsx";
 
-export const HomePage = () => {
+export const DashboardPage = () => {
   return (
     <>
       <Header/>
