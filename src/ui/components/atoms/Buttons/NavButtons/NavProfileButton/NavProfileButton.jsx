14c23fc8f6c480e011c63918b98a8b0bdeba7ac8
styyyxgl <grozav.n900@gmail.com>
2024-08-16 00:28:14 +0300

    feat: add nav profile button dropdown

--- src/ui/components/atoms/Buttons/NavButtons/NavProfileButton/NavProfileButton.jsx
+++ src/ui/components/atoms/Buttons/NavButtons/NavProfileButton/NavProfileButton.jsx
@@ -2,8 +2,25 @@ import "./NavProfileButton.css";
 
 export const NavProfileButton = ({onClick, img}) => {
   return (
-    <button className={'nav-profile-button'} onClick={onClick}>
-      <img src={img} alt={''}/>
-    </button>
+    <div className={'dropdown'}>
+      <button className={'nav-profile-button'} onClick={onClick}>
+        <img src={img} alt={''}/>
+      </button>
+      <nav className="dropdown-content">
+        <legend>User profile</legend>
+        <ul>
+          <li>
+            <h5>Username</h5>
+          </li>
+            <hr/>
+          <li>
+            <button>My profile</button>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
