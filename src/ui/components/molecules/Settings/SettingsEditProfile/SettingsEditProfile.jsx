d832f5ccd3021d01a046a418a5151953e40c5f63
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-17 19:49:50 +0300

    fix: settings edit profile component

--- src/ui/components/molecules/Settings/SettingsEditProfile/SettingsEditProfile.jsx
+++ src/ui/components/molecules/Settings/SettingsEditProfile/SettingsEditProfile.jsx
@@ -1,69 +1,71 @@
 import {SettingsInputBox} from "/src/ui/components/molecules/SettingsInputBox/SettingsInputBox.jsx";
+import {SettingsSaveButton} from "/src/ui/components/atoms/Buttons/SettingsSaveButton/SettingsSaveButton.jsx";
 
 import "./SettingsEditProfile.css";
 
 export const EditProfile = () => {
   return (
     <div className={'edit-profile-container'}>
-      <div className={'edit-profile-img'}>
-        <img src="/src/assets/img/navbar/profile.svg" alt="Profile Avatar"/>
+      <div className={'edit-profile-info-container'}>
+        <div className={'edit-profile-img'}>
+          <img src="/src/assets/img/navbar/profile.svg" alt="Profile Avatar"/>
+        </div>
+        <div className={'edit-profile-info'}>
+          <ul className={'edit-profile-list'}>
+            <SettingsInputBox
+              title={'Your Name'}
+              type={'text'}
+              placeholder={'your name'}
+            />
+            <SettingsInputBox
+              title={'Last Name'}
+              type={'text'}
+              placeholder={'your last name'}
+            />
+            <SettingsInputBox
+              title={'Your Email'}
+              type={'email'}
+              placeholder={'your email'}
+            />
+            <SettingsInputBox
+              title={'Password'}
+              type={'password'}
+              placeholder={'your password'}
+            />
+            <SettingsInputBox
+              title={'Date of Birth'}
+              type={'text'}
+              placeholder={'your birthday'}
+            />
+            <SettingsInputBox
+              title={'Present address'}
+              type={'text'}
+              placeholder={'your address'}
+            />
+            <SettingsInputBox
+              title={'Permanent Address'}
+              type={'text'}
+              placeholder={'your address'}
+            />
+            <SettingsInputBox
+              title={'City'}
+              type={'text'}
+              placeholder={'your city'}
+            />
+            <SettingsInputBox
+              title={'Postal Code'}
+              type={'text'}
+              placeholder={'your code'}
+            />
+            <SettingsInputBox
+              title={'Country'}
+              type={'text'}
+              placeholder={'your country'}
+            />
+          </ul>
+        </div>
       </div>
-      <div className={'edit-profile-info'}>
-        <ul className={'edit-profile-list'}>
-          <SettingsInputBox
-            title={'Your Name'}
-            type={'text'}
-            placeholder={'your name'}
-          />
-          <SettingsInputBox
-            title={'Your Email'}
-            type={'email'}
-            placeholder={'your email'}
-          />
-          <SettingsInputBox
-            title={'Date of Birth'}
-            type={'text'}
-            placeholder={'your birthday'}
-          />
-          <SettingsInputBox
-            title={'Permanent Address'}
-            type={'text'}
-            placeholder={'your address'}
-          />
-          <SettingsInputBox
-            title={'Postal Code'}
-            type={'text'}
-            placeholder={'your code'}
-          />
-        </ul>
-        <ul className={'edit-profile-list'}>
-          <SettingsInputBox
-            title={'Last Name'}
-            type={'text'}
-            placeholder={'your last name'}
-          />
-          <SettingsInputBox
-            title={'Password'}
-            type={'password'}
-            placeholder={'your password'}
-          />
-          <SettingsInputBox
-            title={'Present address'}
-            type={'text'}
-            placeholder={'your address'}
-          />
-          <SettingsInputBox
-            title={'City'}
-            type={'text'}
-            placeholder={'your city'}
-          />
-          <SettingsInputBox
-            title={'Country'}
-            type={'text'}
-            placeholder={'your country'}
-          />
-        </ul>
-      </div>
+      <SettingsSaveButton label={'Save'}/>
     </div>
   )
 }
