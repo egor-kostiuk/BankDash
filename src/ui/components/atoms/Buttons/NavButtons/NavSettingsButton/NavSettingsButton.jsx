3cd1fe3a8873067033dae8ac1c60b28f71593108
styyyxgl <grozav.n900@gmail.com>
2024-08-20 15:21:21 +0300

    feat: add nav notifications dropdown

--- src/ui/components/atoms/Buttons/NavButtons/NavSettingsButton/NavSettingsButton.jsx
+++ src/ui/components/atoms/Buttons/NavButtons/NavSettingsButton/NavSettingsButton.jsx
@@ -1,8 +1,43 @@
+import {useState} from "react";
+
 import "./NavSettingsButton.css";
 
-export const NavSettingsButton = ({onClick, img}) => {
+export const NavSettingsButton = ({img}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <button className={'nav-settings-button'} onClick={onClick}>
-      <img src={img} alt={''}/>
-    </button>  )
+    <div className={'notifications-dropdown'}>
+      <button className={'nav-settings-button'} onClick={toggleDropdown}>
+        <img src={img} alt={''}/>
+      </button>
+      <nav className={`notifications-dropdown-content ${isOpen ? 'open' : ''}`}>
+        <div className={'all-notifications-box'}>
+          <div>
+            <h6 className={'all-notifications'}>All Notifications</h6>
+            <p className={'unread-messages'}>You have 3 unread messages</p>
+          </div>
+        </div>
+
+        <hr className={'top-notification-hr'}/>
+
+        <div>
+          <div>Transaction Successful</div>
+          <div>Low Balance Alert</div>
+        </div>
+
+        <div>
+          <button>View all</button>
+        </div>
+      </nav>
+      {isOpen && <div className="dropdown-overlay" onClick={closeDropdown}></div>}
+    </div>
+  )
 }
