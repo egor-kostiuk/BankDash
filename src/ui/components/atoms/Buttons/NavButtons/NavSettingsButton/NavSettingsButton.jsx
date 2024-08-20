927a1106d67bc188ab10a620309c15d31691002c
styyyxgl <grozav.n900@gmail.com>
2024-08-20 15:07:26 +0300

    Revert "fix: nav notifications add dropdown with anim"
    This reverts commit cac8e385a1d9802302648bbd50df33270ee41491.

--- src/ui/components/atoms/Buttons/NavButtons/NavSettingsButton/NavSettingsButton.jsx
+++ src/ui/components/atoms/Buttons/NavButtons/NavSettingsButton/NavSettingsButton.jsx
@@ -1,43 +1,8 @@
-import {useState} from "react";
-
 import "./NavSettingsButton.css";
 
-export const NavSettingsButton = ({img}) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeDropdown = () => {
-    setIsOpen(false);
-  };
-
+export const NavSettingsButton = ({onClick, img}) => {
   return (
-    <div className={'notifications-dropdown'}>
-      <button className={'nav-settings-button'} onClick={toggleDropdown}>
-        <img src={img} alt={''}/>
-      </button>
-      <nav className={`notifications-dropdown-content ${isOpen ? 'open' : ''}`}>
-        <div className={'all-notifications-box'}>
-          <div>
-            <h6 className={'all-notifications'}>All Notifications</h6>
-            <p className={'unread-messages'}>You have 3 unread messages</p>
-          </div>
-        </div>
-
-        <hr className={'top-notification-hr'}/>
-
-        <div>
-          <div>Transaction Successful</div>
-          <div>Low Balance Alert</div>
-        </div>
-
-        <div>
-          <button>View all</button>
-        </div>
-      </nav>
-      {isOpen && <div className="dropdown-overlay" onClick={closeDropdown}></div>}
-    </div>
-  )
+    <button className={'nav-settings-button'} onClick={onClick}>
+      <img src={img} alt={''}/>
+    </button>  )
 }
