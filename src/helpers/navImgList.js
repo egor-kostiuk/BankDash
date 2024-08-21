8fe0b0156dbf31456d88869251f0e5df11342705
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-21 17:39:13 +0300

    feat: add nav img list TODO

--- src/helpers/navImgList.js
+++ src/helpers/navImgList.js
@@ -1,12 +1,12 @@
 import magnifyingGlassImg from '/src/assets/img/navbar/magnifying-glass.svg'
 import notificationsImg from '/src/assets/img/navbar/notifications.svg'
-import profileImg from '/src/assets/img/navbar/profile.svg'
+// import profileImg from '/src/assets/img/profile.svg'
 import settingsImg from '/src/assets/img/navbar/settings.svg'
 
 
 export const navImgList = {
   magnifyingGlass: magnifyingGlassImg,
   notifications: notificationsImg,
-  profile: profileImg,
+  // profile: profileImg, // TODO: change nav profile img
   settings: settingsImg,
 }
