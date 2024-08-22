359f3dab42497620c4575913cbff22b4132d8679
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-22 18:03:19 +0300

    feat: add toasts for login page

--- src/ui/components/organisms/AuthForm/Login/LoginForm.jsx
+++ src/ui/components/organisms/AuthForm/Login/LoginForm.jsx
@@ -1,10 +1,12 @@
 import {useLogin} from "./LoginForm.js";
 import {Link} from "react-router-dom";
+import {ToastContainer } from "react-toastify";
 
 import {InputBox} from "/src/ui/components/molecules/InputBox/InputBox.jsx";
 import {AuthButton} from "/src/ui/components/atoms/Buttons/AuthButton/AuthButton.jsx";
 
 import "../Form.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export const LoginForm = () => {
   const {setEmail, setPassword, handleSubmit} = useLogin();
@@ -30,6 +32,12 @@ export const LoginForm = () => {
           Don't have an account ? <Link to="/sign-up">Sign Up</Link>
         </p>
       </form>
+      <ToastContainer
+        style={{top: '-100px'}}
+        position={"top-center"}
+        autoClose={2000}
+        pauseOnHover={false}
+      />
     </div>
   )
 }
