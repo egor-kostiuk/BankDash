1e69214e870dab1ea2f027ad4d21ec1b11c10bcd
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-26 17:32:41 +0300

    translate: firebase documentation

--- src/services/api/firebase.js
+++ src/services/api/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
 
-// Firebase конфігурація
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDgty4m1--Rus0HGkg-jPMVZuq94badEP4",
   authDomain: "bankdash-f1454.firebaseapp.com",
@@ -14,36 +14,32 @@ const firebaseConfig = {
   appId: "1:1059876621429:web:2c5b5554f5742642f03c3c"
 };
 
-// Ініціалізація Firebase
+// Firebase initialize
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-// Функція для завантаження зображення в Firebase Storage
+// Function to upload photo to Firebase Storage
 export async function upload(file, currentUser, setLoading) {
   const fileRef = ref(storage, `${currentUser.uid}.png`);
 
   setLoading(true);
 
   try {
-    // Завантаження файлу в Firebase Storage
     await uploadBytes(fileRef, file);
 
-    // Отримання URL завантаженого зображення
     const photoURL = await getDownloadURL(fileRef);
 
-    // Оновлення профілю користувача з новим URL зображення
     await updateProfile(currentUser, { photoURL });
 
     setLoading(false);
 
-    // Повертаємо URL для подальшого використання
     return photoURL;
   } catch (error) {
-    console.error("Помилка під час завантаження фото: ", error);
+    console.error("Error loading photo: ", error);
     setLoading(false);
-    alert("Не вдалося завантажити фото. Спробуйте ще раз.");
+    alert("Failed to upload photo. Try again.");
     return null;
   }
 }
