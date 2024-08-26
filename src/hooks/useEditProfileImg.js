29c2c90b6d3327d71799ddc14c0dcd4014c31e94
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-26 17:30:06 +0300

    update: edit profile img hook

--- src/hooks/useEditProfileImg.js
+++ src/hooks/useEditProfileImg.js
@@ -1,23 +1,37 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
+import { auth, upload } from "/src/services/api/firebase.js";
 
 export const useEditProfileImg = () => {
   const [profileImg, setProfileImg] = useState("/src/assets/img/default-profile-img.jpg");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user && user.photoURL) {
+      setProfileImg(user.photoURL);
+    }
+  }, []);
 
   const handleButtonClick = () => {
-    // console.log("Button clicked");
     document.getElementById('fileInput').click();
   };
 
-  const handleImageChange = (e) => {
-    // console.log("File input changed");
+  const handleImageChange = async (e) => {
     const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
         setProfileImg(reader.result);
-        // console.log("Image loaded");
       };
       reader.readAsDataURL(file);
+
+      const user = auth.currentUser;
+      if (user) {
+        const newPhotoURL = await upload(file, user, setLoading);
+        if (newPhotoURL) {
+          setProfileImg(newPhotoURL);
+        }
+      }
     }
   };
 
@@ -25,5 +39,6 @@ export const useEditProfileImg = () => {
     profileImg,
     handleButtonClick,
     handleImageChange,
+    loading,
   };
 };
