5baf634e0e122af547271ad3090d08215f6b41ae
styyyxgl <grozav.n900@gmail.com>
2024-08-27 18:28:54 +0300

    feat: nav button anim

--- src/ui/components/atoms/Buttons/NavButtons/NavProfileButton/NavProfileButton.jsx
+++ src/ui/components/atoms/Buttons/NavButtons/NavProfileButton/NavProfileButton.jsx
@@ -8,18 +8,26 @@ import "./NavProfileButon.css";
 export const NavProfileButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { profileImg } = useEditProfileImg();
+  const [isFilling, setIsFilling] = useState(false);
 
+  // Function for dropdown open and click styles
   const toggleDropdown = () => {
     setIsOpen(prev => !prev);
+    setIsFilling(true);
+
+    setTimeout(() => {
+      setIsFilling(false);
+    }, 500);
   };
 
   return (
     <div style={{position: 'relative'}}>
       <button
-        className={'nav-profile-button'}
+        className={`nav-profile-button ${isFilling ? 'filling' : ''}`}
         onClick={toggleDropdown}
       >
         <img src={profileImg} alt="" />
+        {isFilling && <div className="fill-overlay"></div>}
       </button>
       <div className={`nav-profile-dropdown ${isOpen ? 'open' : ''}`}>
         <NavProfileDropdown img={profileImg} closeDropdown={() => setIsOpen(false)} />
