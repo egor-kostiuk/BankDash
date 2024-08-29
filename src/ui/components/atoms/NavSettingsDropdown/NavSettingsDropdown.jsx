34d0571ab3f0e7e3ca44e4c233cc4428f2414a6c
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-29 22:10:48 +0300

    fix: nav settings dropdown img

--- src/ui/components/atoms/NavSettingsDropdown/NavSettingsDropdown.jsx
+++ src/ui/components/atoms/NavSettingsDropdown/NavSettingsDropdown.jsx
@@ -1,8 +1,7 @@
 import { NavSettingsDropdownContent } from "../NavSettingsDropdownContent/NavSettingsDropdownContent.jsx";
-import {navImgList} from "/src/helpers/navImgList.js";
+import { navSettingsDropdownImgList } from "/src/helpers/navSettingsDropdownImgList.js";
 
 import  "./NavSettingsDropdown.css"
-import {navImgSvgList} from "../../../../helpers/navImgSvgList.js";
 
 
 export const NavSettingsDropdown = () => {
@@ -12,17 +11,13 @@ export const NavSettingsDropdown = () => {
         <h6 className={'h6-notifications'}>All Notifications</h6>
         <p className={'unread-messages'}>You have 2 unread messages</p>
       </div>
-
-  <hr className={'notifications-hr'}/>
-
+    <hr className={'notifications-hr'}/>
       <div className={'notifications'}>
-        <NavSettingsDropdownContent className={'notifications-unread'} img={navImgList.transactionMoney} title={'Transaction Successful'} time={'Aug 29, 2024 2:51 pm'}/>
-        <NavSettingsDropdownContent className={'notifications-unread'} img={navImgSvgList.lowBalanceAlert} title={'Low Balance Alert'} time={'Aug 29, 2024 12:06 pm'}/>
-        <NavSettingsDropdownContent className={'notifications-read'} img={navImgList.newLoanOffer} title={'New Loan Offer'} time={'Aug 28, 2024 1:51 pm'}/>
+        <NavSettingsDropdownContent className={'notifications-unread'} img={navSettingsDropdownImgList.transactionMoney} title={'Transaction Successful'} time={'Aug 29, 2024 2:51 pm'}/>
+        <NavSettingsDropdownContent className={'notifications-unread'} img={navSettingsDropdownImgList.lowBalanceAlert} title={'Low Balance Alert'} time={'Aug 29, 2024 12:06 pm'}/>
+        <NavSettingsDropdownContent className={'notifications-read'} img={navSettingsDropdownImgList.newLoanOffer} title={'New Loan Offer'} time={'Aug 28, 2024 1:51 pm'}/>
       </div>
-
-  <hr className={'notifications-hr'}/>
-
+    <hr className={'notifications-hr'}/>
    </div>
   );
 };
