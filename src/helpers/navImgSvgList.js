41a67a46a9753b44a9d64f2a437e0326d8af2a96
styyyxgl <grozav.n900@gmail.com>
2024-08-29 21:41:36 +0300

    fix: nav svg extension

--- src/helpers/navImgSvgList.js
+++ src/helpers/navImgSvgList.js
@@ -1,5 +1,9 @@
 import lowBalanceAlertImg from '/src/assets/img/navbarSvg/low-balance-alert.svg'
+import infoMailImg from '/src/assets/img/navbarSvg/info-mail.svg'
+import profileSettingsImg from '/src/assets/img/navbarSvg/profile-settings.svg'
 
 export const navImgSvgList = {
   lowBalanceAlert: lowBalanceAlertImg,
+  infoMail: infoMailImg,
+  profileSettings: profileSettingsImg,
 }
