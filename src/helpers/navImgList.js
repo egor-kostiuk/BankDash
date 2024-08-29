ba92cae0242d5c77ee275b954e1e39eb5e6c078d
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-29 22:09:52 +0300

    fix: nav img lists

--- src/helpers/navImgList.js
+++ src/helpers/navImgList.js
@@ -1,16 +1,11 @@
-import magnifyingGlassImg from '/src/assets/img/navbar/magnifying-glass.svg'
-import notificationsImg from '/src/assets/img/navbar/notifications.svg'
-import profileImg from '/src/assets/img/default-profile-img.jpg'
-import settingsImg from '/src/assets/img/navbar/settings.svg'
-import transactionMoneyImg from '/src/assets/img/navbar/transaction-money.svg'
-import newLoanOfferImg from '/src/assets/img/navbar/new-loan-offer.svg'
-
+import magnifyingGlassImg from '/src/assets/img/navbar/magnifying-glass.svg';
+import notificationsImg from '/src/assets/img/navbar/notifications.svg';
+import profileImg from '/src/assets/img/default-profile-img.jpg';
+import settingsImg from '/src/assets/img/navbar/settings.svg';
 
 export const navImgList = {
   magnifyingGlass: magnifyingGlassImg,
   notifications: notificationsImg,
   profile: profileImg,
   settings: settingsImg,
-  transactionMoney: transactionMoneyImg,
-  newLoanOffer: newLoanOfferImg,
 }
--- src/helpers/navImgSvgList.js
+++
@@ -1,5 +0,0 @@
-import lowBalanceAlertImg from '/src/assets/img/navbarSvg/low-balance-alert.svg'
-
-export const navImgSvgList = {
-  lowBalanceAlert: lowBalanceAlertImg,
-}
