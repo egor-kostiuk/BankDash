ab6f36fb7b3c90e6690ef0d582823b3ff1ade69e
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-30 22:25:30 +0300

    fix: nav profile dropdown info tags error

--- src/ui/components/atoms/NavProfileDropdownInfo/NavProfileDropdownInfo.jsx
+++ src/ui/components/atoms/NavProfileDropdownInfo/NavProfileDropdownInfo.jsx
@@ -8,12 +8,10 @@ export const NavProfileDropdownInfo = ({ img, title, subtitle, email }) => (
         <h6 className={'dropdown-username'}>{title[0]} {title[1]}</h6>
         <h6 className={'dropdown-activity'}>{subtitle}</h6>
         <h6 className={'dropdown-mail'}>
-          <h6 className={'dropdown-mail'}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24">
-              <path fill="none" stroke="currentColor" d="m7 9l3.75 3a2 2 0 0 0 2.5 0L17 9m4 8V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2" />
-            </svg>
-            {email}
-          </h6>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" d="m7 9l3.75 3a2 2 0 0 0 2.5 0L17 9m4 8V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2" />
+          </svg>
+          {email}
         </h6>
       </div>
     </div>
