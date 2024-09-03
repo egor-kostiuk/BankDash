c0b109727ff0ca8f20b56b99dc345c49be5f30e5
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-03 21:24:20 +0300

    feat: add setting date input box

---
+++ src/ui/components/molecules/SettingsDateInputBox/SettingsDateInputBox.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import { SettingsLabel } from "/src/ui/components/atoms/SettingsLabel/SettingsLabel.jsx";
+import { SettingsDateInput } from "/src/ui/components/atoms/SettingsDateInput/SettingsDateInput.jsx";
+
+import "./SettingsDateInputBox.css"
+
+export const SettingsDateInputBox = ({ title, placeholder, onChange }) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    onChange(date);
+  };
+
+  return (
+    <div className={'settings-date-input-box'}>
+      <SettingsLabel title={title} />
+      <SettingsDateInput
+        selectedDate={selectedDate}
+        onChange={handleDateChange}
+        placeholder={placeholder}
+        dateFormat={'yyyy/MM/dd'}
+      />
+    </div>
+  )
+}
