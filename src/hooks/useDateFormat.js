5bb1f35083923eb8965b8e49b5bd1ea4a4a5c80b
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-05 16:06:21 +0300

    fix: birthDate format

--- src/hooks/useDateFormat.js
+++ src/hooks/useDateFormat.js
@@ -1,7 +1,7 @@
 export const useDateFormat = (timestamp) => {
   if (timestamp && timestamp.seconds) {
     const date = new Date(timestamp.seconds * 1000);
-    return date.toISOString().split('T')[0];
+    return date.toISOString().split('T')[0].replace(/-/g, '/');
   }
   return null;
 };
