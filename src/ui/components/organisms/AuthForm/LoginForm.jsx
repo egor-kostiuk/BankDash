6e3afcd61416e08b190ea82d96ced907027122ff
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-06 17:58:17 +0300

    fix: toasts for login page

--- src/ui/components/organisms/AuthForm/LoginForm.jsx
+++ src/ui/components/organisms/AuthForm/LoginForm.jsx
@@ -1,10 +1,12 @@
 import { useLogin } from "/src/hooks/useLogin.js";
 import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import { AuthInputBox } from "/src/ui/components/molecules/AuthInputBox/AuthInputBox.jsx";
 import { AuthButton } from "/src/ui/components/atoms/Buttons/AuthButton/AuthButton.jsx";
 
 import "./Form.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const LoginForm = () => {
   const { setEmail, setPassword, handleSubmit } = useLogin();
@@ -30,6 +32,12 @@ export const LoginForm = () => {
           Don't have an account ? <Link to="/sign-up">Sign Up</Link>
         </p>
       </form>
+      <ToastContainer
+        style={{top: '-100px'}}
+        position={"top-center"}
+        autoClose={2000}
+        pauseOnHover={false}
+      />
     </div>
   )
 }
