28c7e39cd5813f70b866f66a8e547cf96d63478e
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-08 23:29:56 +0300

    update: pages imports

---
+++ src/app/pages/pages.js
@@ -0,0 +1,15 @@
+import { LoginPage } from '/src/app/pages/Auth/LoginPage.jsx';
+import { SignUpPage } from '/src/app/pages/Auth/SignUpPage.jsx';
+import { DashboardPage } from '/src/app/pages/DashboardPage.jsx';
+import { TestPage } from '/src/app/pages/TestPage.jsx';
+import { CardsPage } from '/src/app/pages/CardsPage.jsx'
+import { SettingsPage } from '/src/app/pages/SettingsPage.jsx';
+
+export {
+  LoginPage,
+  SignUpPage,
+  DashboardPage,
+  TestPage,
+  CardsPage,
+  SettingsPage,
+}
