72e0dd7fba3d0c7343731127f5787a8e5c63db85
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-11 21:08:46 +0300

    update: settings chose input box

--- src/ui/components/atoms/SettingsChoseInput/SettingsChoseInput.jsx
+++ src/ui/components/atoms/SettingsChoseInput/SettingsChoseInput.jsx
@@ -1,12 +1,23 @@
-import "./SettingsChoseInput.css";
+import { useState, useMemo } from 'react';
+import Select from 'react-select';
+import countryList from 'react-select-country-list'
 
-export const SettingsChoseInput = ({ type, placeholder, readOnly }) => {
-  return (
-    <input
-      className={'settings-chose-input'}
-      type={type}
-      placeholder={placeholder}
-      readOnly={readOnly}
-    />
-  )
+import './SettingsChoseInput.css';
+
+export const SettingsChoseInput = ({ placeholder }) => {
+  const [value, setValue] = useState('')
+  const options = useMemo(() => countryList().getData(), [])
+
+  const changeHandler = value => {
+    setValue(value)
+  }
+
+  return <Select
+    classNamePrefix={'country-select'}
+    options={options}
+    value={value}
+    placeholder={placeholder}
+    onChange={changeHandler}
+    unstyled
+  />
 }
--- src/ui/components/molecules/SettingsChoseInputBox/SettingsChoseInputBox.jsx
+++ src/ui/components/molecules/SettingsChoseInputBox/SettingsChoseInputBox.jsx
@@ -1,66 +1,14 @@
-import { useState, useEffect } from "react";
-
 import { SettingsLabel } from "/src/ui/components/atoms/SettingsLabel/SettingsLabel.jsx";
 import { SettingsChoseInput } from "/src/ui/components/atoms/SettingsChoseInput/SettingsChoseInput.jsx";
-import { countryOptions } from "/src/helpers/countrysList.js";
 
 import "./SettingsChoseInputBox.css";
 
-export const SettingsChoseInputBox = ({ title, placeholder, setCountry }) => {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState(placeholder);
-  const [searchTerm, setSearchTerm] = useState('');
-  
-  const filteredCountries = countryOptions.filter(country =>
-    country.label.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
-  useEffect(() => {
-    setSelectedCountry(placeholder);
-  }, [placeholder]);
-
-  const toggleDropdown = () => setDropdownVisible(!isDropdownVisible);
-
-  const handleCountrySelect = (country) => {
-    setSelectedCountry(country.label);
-    setCountry(country.label);
-    setSearchTerm('');
-    setDropdownVisible(false);
-  };
-
+export const SettingsChoseInputBox = ({ title, placeholder }) => {
   return (
     <div className={'settings-chose-input-box'}>
       <SettingsLabel title={title}/>
-      <button onClick={toggleDropdown}>
-        <SettingsChoseInput readOnly={true} placeholder={selectedCountry} />
-      </button>
-      {isDropdownVisible && (
-        <div className={'dropdown'}>
-          <div className={'dropdown-content'}>
-            <div className={'dropdown-content-input-box'}>
-              <input
-                type={'text'}
-                placeholder={'Enter country'}
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-              <span></span>
-            </div>
-            <ul className={'dropdown-countries-list'}>
-              {filteredCountries.length > 0 ? (
-                filteredCountries.map((country) => (
-                  <li key={country.value} onClick={() => handleCountrySelect(country)}>
-                    {country.label}
-                  </li>
-                ))
-              ) : (
-                <li>No matching countries</li>
-              )}
-            </ul>
-          </div>
-        </div>
-      )}
-      {isDropdownVisible && <div className={'dropdown-overlay'} onClick={() => setDropdownVisible(false)} ></div>}
+      <SettingsChoseInput placeholder={placeholder}/>
     </div>
   )
 }
