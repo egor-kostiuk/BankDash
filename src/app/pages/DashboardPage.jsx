855d90fdcb1606bafd4f30ea9d3d5fffe100e010
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-17 21:17:44 +0300

    update: dasboard page with currency provider

--- src/app/pages/DashboardPage.jsx
+++ src/app/pages/DashboardPage.jsx
@@ -1,11 +1,24 @@
 import { Main } from "/src/ui/components/templates/Main/Main.jsx";
 import { Header } from "/src/ui/components/templates/Header/Header.jsx";
 
+import { useEditProfile } from '/src/hooks/useEditProfile.js';
+import { CurrencyWrapper } from '/src/utils/CurrencyProvider/CurrencyWrapper.jsx';
+import { CurrencyProvider } from '/src/utils/CurrencyProvider/CurrencyContext.jsx';
+
+import { CurrencyText } from '/src/utils/CurrencyText.jsx'; // Test component
+
+
 export const DashboardPage = () => {
+  const { userDetails } = useEditProfile();
+
   return (
-    <>
-      <Header title={'Overview'}/>
-      <Main/>
-    </>
+    <CurrencyProvider currency={userDetails?.currency}>
+      <CurrencyWrapper>
+        <Header title={'Overview'}/>
+        <Main>
+          <CurrencyText>{2100}</CurrencyText>
+        </Main>
+      </CurrencyWrapper>
+    </CurrencyProvider>
   )
 }
