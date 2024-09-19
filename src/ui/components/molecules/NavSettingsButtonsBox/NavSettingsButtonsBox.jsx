bc0a366978b4cc95b61e175e30259af41271ccc3
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-19 17:06:40 +0300

    update: nav settings button box

--- src/ui/components/molecules/NavSettingsButtonsBox/NavSettingsButtonsBox.jsx
+++ src/ui/components/molecules/NavSettingsButtonsBox/NavSettingsButtonsBox.jsx
@@ -1,17 +1,43 @@
-import { NavSettingsButton } from "/src/ui/components/atoms/Buttons/NavButtons/NavSettingsButton/NavSettingsButton.jsx";
-import { NavSettingsDropdown } from "/src/ui/components/atoms/NavSettingsDropdown/NavSettingsDropdown.jsx";
+import { NavSettingsButton } from '/src/ui/components/atoms/Buttons/NavButtons/NavSettingsButton/NavSettingsButton.jsx';
+import { NavSetAndNotDropdown } from '/src/ui/components/atoms/NavSetAndNotDropdown/NavSetAndNotDropdown.jsx';
+import { NavNotificationDropdownCard } from '/src/ui/components/atoms/NavNotificationDropdownCard/NavNotificationDropdownCard.jsx';
+import { notificationList } from '/src/helpers/notificationList.js';
 
-import "./NavSettingsButtonsBox.css";
+import './NavSettingsButtonsBox.css';
 
 export const NavSettingsButtonsBox = ({ onClick, img }) => {
   return (
     <div className={'nav-button-wrapper'}>
+      {/* Settings button */}
       <NavSettingsButton
         onClick={onClick}
-        img={img[0]}
-      />
-      <NavSettingsButton onClick={onClick} img={img[1]}>
-        <NavSettingsDropdown/>
+        img={img[0]}>
+        <NavSetAndNotDropdown
+          title={'Settings'}>
+          box
+        </NavSetAndNotDropdown>
+      </NavSettingsButton>
+
+      {/* Notification button */}
+      <NavSettingsButton
+        onClick={onClick}
+        img={img[1]}>
+        <NavSetAndNotDropdown
+          title={'All Notifications'}
+          subtitle={`You have ${2} unread messages`}
+          buttonTitle={'View all'}>
+
+          {/* Creat notification cards */}
+          {notificationList.map((notification, index) => (
+            <NavNotificationDropdownCard
+              key={index}
+              className={notification.isUnread ? "notifications-unread" : "notifications-read"}
+              img={notification.img}
+              title={notification.title}
+              time={notification.time}
+            />
+          ))}
+        </NavSetAndNotDropdown>
       </NavSettingsButton>
     </div>
   )
