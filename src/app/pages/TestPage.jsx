bd85bb8a6aa2e3d19a11e541209599f48f68e4a7
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-19 19:09:54 +0300

    fix: deleted theme switch

--- src/app/pages/TestPage.jsx
+++ src/app/pages/TestPage.jsx
@@ -2,33 +2,11 @@ import { Main } from "/src/ui/components/templates/Main/Main.jsx";
 import { Header } from "/src/ui/components/templates/Header/Header.jsx";
 
 export const TestPage = () => {
-  const switchTheme = (theme) => {
-    document.documentElement.setAttribute('data-theme', theme);
-  };
 
   return (
     <>
       <Header title={'Test'}/>
-      <Main>
-        <button
-          style={{width: 100, height: 50, backgroundColor: 'wheat'}}
-          onClick={() => switchTheme('light')}
-        >
-          light
-        </button>
-        <button
-          style={{width: 100, height: 50, backgroundColor: 'darkgray'}}
-          onClick={() => switchTheme('dark')}
-        >
-          dark
-        </button>
-        <button
-          style={{width: 100, height: 50, backgroundColor: 'deepskyblue'}}
-          onClick={() => switchTheme('ocean')}
-        >
-          ocean
-        </button>
-      </Main>
+      <Main/>
     </>
   )
 }
