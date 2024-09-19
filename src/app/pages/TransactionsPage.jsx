64a28bbe1774ef406f4ab859873bcad6390ed30d
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-19 19:16:30 +0300

    update: transactions page imports

---
+++ src/app/pages/TransactionsPage.jsx
@@ -0,0 +1,12 @@
+import { Main } from '/src/ui/components/templates/Main/Main.jsx';
+import { Header } from '/src/ui/components/templates/Header/Header.jsx';
+
+export const TransactionsPage = () => {
+
+  return (
+    <>
+      <Header title={'Transactions'}/>
+      <Main/>
+    </>
+  )
+}
