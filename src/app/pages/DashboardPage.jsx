61aceb93e11bada51f48db241a9631ff64341dfc
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-20 14:30:02 +0300

    update: dasboard page

--- src/app/pages/DashboardPage.jsx
+++ src/app/pages/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { Header } from '/src/ui/components/templates/Header/Header.jsx';
 import { useEditProfile } from '/src/hooks/useEditProfile.js';
 import { CurrencyWrapper } from '/src/utils/CurrencyProvider/CurrencyWrapper.jsx';
 import { CurrencyProvider } from '/src/utils/CurrencyProvider/CurrencyContext.jsx';
-import { CurrencyText } from '/src/utils/CurrencyText.jsx'; // Test component
+import { CardList } from '/src/ui/CardsList.jsx'; // Test component
 
 
 export const DashboardPage = () => {
@@ -15,7 +15,7 @@ export const DashboardPage = () => {
       <CurrencyWrapper>
         <Header title={'Overview'}/>
         <Main>
-          <CurrencyText>{2100}</CurrencyText>
+          <CardList/>
         </Main>
       </CurrencyWrapper>
     </CurrencyProvider>
