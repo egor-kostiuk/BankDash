bfe4e95a50a6e4e40dec285de0d7cea016a309ed
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-21 17:44:53 +0300

    fix: cards page

--- src/app/pages/CardsPage.jsx
+++ src/app/pages/CardsPage.jsx
@@ -1,18 +1,19 @@
+import { CurrencyProvider } from '/src/utils/CurrencyProvider/CurrencyContext.jsx';
+import { CurrencyWrapper } from '/src/utils/CurrencyProvider/CurrencyWrapper.jsx';
+
 import { Main } from '/src/ui/components/templates/Main/Main.jsx';
 import { Header } from '/src/ui/components/templates/Header/Header.jsx';
 import { CardsContent } from '/src/ui/components/organisms/Cards/CardsContent.jsx';
-import {CurrencyProvider} from "../../utils/CurrencyProvider/CurrencyContext.jsx";
-import {CurrencyWrapper} from "../../utils/CurrencyProvider/CurrencyWrapper.jsx";
 
 export const CardsPage = () => {
   return (
     <>
       <CurrencyProvider>
         <CurrencyWrapper>
-      <Header title={'Credit Cards'}/>
-      <Main>
-        <CardsContent/>
-      </Main>
+          <Header title={'Credit Cards'}/>
+          <Main>
+            <CardsContent/>
+          </Main>
         </CurrencyWrapper>
       </CurrencyProvider>
     </>
