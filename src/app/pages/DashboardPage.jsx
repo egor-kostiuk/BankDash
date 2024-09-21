2567d087759b9b9d4686cbf875ba1702ae38c4dc
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-21 12:16:59 +0300

    update: dasboard page

--- src/app/pages/DashboardPage.jsx
+++ src/app/pages/DashboardPage.jsx
@@ -4,8 +4,6 @@ import { Header } from '/src/ui/components/templates/Header/Header.jsx';
 import { useEditProfile } from '/src/hooks/useEditProfile.js';
 import { CurrencyWrapper } from '/src/utils/CurrencyProvider/CurrencyWrapper.jsx';
 import { CurrencyProvider } from '/src/utils/CurrencyProvider/CurrencyContext.jsx';
-import { CardList } from '/src/ui/CardsList.jsx'; // Test component
-
 
 export const DashboardPage = () => {
   const { userDetails } = useEditProfile();
@@ -15,7 +13,6 @@ export const DashboardPage = () => {
       <CurrencyWrapper>
         <Header title={'Overview'}/>
         <Main>
-          <CardList/>
         </Main>
       </CurrencyWrapper>
     </CurrencyProvider>
