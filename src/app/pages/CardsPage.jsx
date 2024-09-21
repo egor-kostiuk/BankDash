3411b8436bfa1e480afba60a4c39b0771be41569
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-21 17:20:27 +0300

    update: cards page

--- src/app/pages/CardsPage.jsx
+++ src/app/pages/CardsPage.jsx
@@ -1,14 +1,20 @@
 import { Main } from '/src/ui/components/templates/Main/Main.jsx';
 import { Header } from '/src/ui/components/templates/Header/Header.jsx';
 import { CardsContent } from '/src/ui/components/organisms/Cards/CardsContent.jsx';
+import {CurrencyProvider} from "../../utils/CurrencyProvider/CurrencyContext.jsx";
+import {CurrencyWrapper} from "../../utils/CurrencyProvider/CurrencyWrapper.jsx";
 
 export const CardsPage = () => {
   return (
     <>
+      <CurrencyProvider>
+        <CurrencyWrapper>
       <Header title={'Credit Cards'}/>
       <Main>
         <CardsContent/>
       </Main>
+        </CurrencyWrapper>
+      </CurrencyProvider>
     </>
   )
 }
