9da2101be81f1a56b0edecd480c7dfd878acd6e7
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-21 17:20:21 +0300

    update: cards content

--- src/ui/components/organisms/Cards/CardsContent.jsx
+++ src/ui/components/organisms/Cards/CardsContent.jsx
@@ -1,9 +1,14 @@
 import { CardExpenseStatsBox } from '/src/ui/components/molecules/CardExpenseStatsBox/CardExpenseStatsBox.jsx';
 
+import { NewCardBox } from "../../molecules/NewCardBox/NewCardBox.jsx";
+import { CardsListBox } from "../../molecules/CardsList/CardsList.jsx";
+
 export const CardsContent = () => {
   return (
-    <div>
+    <div style={{display: 'flex', flexDirection: 'column', gap: 50}}> {/* TODO: delete div */}
+      <CardsListBox/>
       <CardExpenseStatsBox/>
+      <NewCardBox/>
     </div>
   )
 }
