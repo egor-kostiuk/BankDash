b5d8f1f9646774b1d53744c5516703b87e7ebb54
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-22 15:52:27 +0300

    update: card component & styles

--- src/ui/components/atoms/Card/Card.jsx
+++ src/ui/components/atoms/Card/Card.jsx
@@ -1,16 +1,17 @@
 import { CurrencyText } from '/src/utils/CurrencyText.jsx';
+import { bankImgList } from '/src/helpers/bankImgList.js';
 
 import './Card.css';
 
-export const Card = ({ number, balance , color, name }) => {
+export const Card = ({ number, type, balance, name, bank, term }) => {
   return (
-    <div className={'card-wrapper'}>
+    <div className={`card-wrapper ${type}`}>
       <div className={'card-content-title'}>
         <div className={'card-content-title-balance'}>
           <span>Balance</span>
           <CurrencyText>{balance}</CurrencyText>
         </div>
-        <img src={`/src/assets/img/card/card-chip-${color}.svg`} alt={'chip'}/>
+        <img src={bankImgList.cardChipWhiteImg} alt={'chip'}/>
       </div>
       <div className={'card-content-main'}>
         <div className={'card-content-main-data'}>
@@ -19,12 +20,20 @@ export const Card = ({ number, balance , color, name }) => {
         </div>
         <div className={'card-content-main-data'}>
           <span>VALID THRU</span>
-          <span>12/22</span>
+          <span>{term}</span>
+        </div>
+        <div className={'card-content-main-data'}>
+          <span>BANK</span>
+          <span>{bank}</span>
         </div>
       </div>
       <div className={'card-content-number'}>
         <span>{number.replace(/(.{4})/g, '$1 ')}</span>
-        <img src={`/src/assets/img/card/card-bank-${color}.svg`} alt={'bank'}/>
+        {bank === 'ABM' || bank === 'BRC' ? (
+          <img style={{width: 44, height: 30}} src={bankImgList.visaImg} alt={'bank'}/>
+        ) : (
+          <img src={bankImgList.mastercardWhiteImg} alt={'bank'}/>
+        )}
       </div>
     </div>
   )
