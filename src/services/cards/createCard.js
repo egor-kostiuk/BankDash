4c385ec6445386daf9ab896521d68260fecb5e5d
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-22 15:49:02 +0300

    update: create card props

--- src/services/cards/createCard.js
+++ src/services/cards/createCard.js
@@ -1,7 +1,10 @@
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '/src/services/api/firebase.js';
 
-export const createCard = async (userId, initialBalance = 0, initialType , initialNumber, initialName) => {
+export const createCard = async (userId, initialBalance = 0, initialType , initialNumber, initialName, initialBank) => {
+  const monthTerm = Math.floor(Math.random() * 12 + 1);
+  const yearTerm = Math.floor(Math.random() * 6 + 24);
+
   try {
     const cardData = {
       userId,
@@ -10,6 +13,8 @@ export const createCard = async (userId, initialBalance = 0, initialType , initi
       createdAt: new Date(),
       cardNumber: initialNumber,
       cardName: initialName,
+      cardBank: initialBank,
+      cardTerm: `${monthTerm < 10 ? `0${monthTerm}` : monthTerm}/${yearTerm}`,
     };
     const docRef = await addDoc(collection(db, 'cards'), cardData);
     return docRef.id;
