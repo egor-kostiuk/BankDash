5ef4a7645c49b83b05b7d71b5bb1c06a57d890f7
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-23 16:53:42 +0300

    update: banks list

--- src/helpers/banksList.js
+++ src/helpers/banksList.js
@@ -1,6 +1,6 @@
 export const banksList = [
-  { name: 'DBL Bank', color: 'var(--second-blue)' },
-  { name: 'ABM Bank', color: 'var(--green)' },
-  { name: 'MCP Bank', color: 'var(--yellow)' },
-  { name: 'BRC Bank', color: 'var(--second-red)' },
+  { name: 'DBL Bank' },
+  { name: 'ABM Bank' },
+  { name: 'MCP Bank' },
+  { name: 'BRC Bank' },
 ];
