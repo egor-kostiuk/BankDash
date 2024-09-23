d8f4d6d776ec00701946396cce473d41a82bef66
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-23 17:02:37 +0300

    feat: add cards settings list

---
+++ src/ui/components/molecules/CardsSettingsList/CardsSettingsList.jsx
@@ -0,0 +1,28 @@
+import { auth } from '/src/services/api/firebase.js';
+import { useCards } from '/src/services/cards/hooks/useCards.js';
+
+import { ContainerTitle } from '/src/ui/components/atoms/ContainerTitle/ContainerTitle.jsx';
+import { CardSettingsBox } from '/src/ui/components/atoms/CardSettingsBox/CardSettingsBox.jsx';
+
+import './CardsSettingsList.css';
+
+export const CardsSettingsList = () => {
+  const user = auth.currentUser;
+  const cards = useCards(user?.uid);
+
+  return (
+    <div style={{flexGrow: 1}}>
+      <ContainerTitle title={'Card List'}/>
+      <ul className={'cards-settings-list'}>
+        {cards.map(card => (
+          <CardSettingsBox
+            key={card.id}
+            cardType={card.cardType}
+            cardBank={card.cardBank}
+            cardName={card.cardName}
+            cardNumber={card.cardNumber}/>
+        ))}
+      </ul>
+    </div>
+  )
+}
