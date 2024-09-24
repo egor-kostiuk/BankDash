0067d3b1224742cba0f77289c84b2f757221bb18
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-24 19:59:28 +0300

    update: card settings box props

--- src/ui/components/atoms/CardSettingsBox/CardSettingsBox.jsx
+++ src/ui/components/atoms/CardSettingsBox/CardSettingsBox.jsx
@@ -2,19 +2,19 @@ import { CardSettingsBoxIcon } from '/src/assets/img/card/CardSettingsBoxIcon.js
 
 import './CardSettingsBox.css';
 
-export const CardSettingsBox = ({ cardType, cardBank, cardName, cardNumber }) => {
+export const CardSettingsBox = ({ cardType, cardBank, cardName, cardNumber, cardStatus }) => {
   return (
     <div className={'card-setting-box'}>
       <div className={`img-box ${cardBank}`}>
         <CardSettingsBoxIcon className={cardBank}/>
       </div>
       <div className={'card-settings-content'}>
-        <span>Card Type</span>
-        <span style={{WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent'}} className={cardType}>{cardType}</span>
-      </div>
-      <div className={'card-settings-content'}>
         <span>Bank</span>
         <span style={{background: 'transparent'}} className={cardBank}>{cardBank} Bank</span>
+      </div>
+      <div className={'card-settings-content'}>
+        <span>Card Type</span>
+        <span style={{WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent'}} className={cardType}>{cardType}</span>
       </div>
       <div className={'card-settings-content'}>
         <span>Card Number</span>
@@ -22,7 +22,11 @@ export const CardSettingsBox = ({ cardType, cardBank, cardName, cardNumber }) =>
       </div>
       <div className={'card-settings-content'}>
         <span>Card Name</span>
-        <span>{cardName}</span>
+        <span className={'card-name'}>{cardName}</span>
+      </div>
+      <div className={'card-settings-content'}>
+        <span>Status</span>
+        <span style={{color: `${cardStatus === 'Active' ? '#00c700' : 'red'}`}}>{cardStatus}</span>
       </div>
       <div className={'button'}>
         <button>View Details</button>
