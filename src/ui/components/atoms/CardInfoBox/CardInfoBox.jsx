4bb976bb96de64240af7d9c9cfdc3f172080ea90
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-24 20:14:00 +0300

    update: rename card settings box -> card info box

--- src/ui/components/atoms/CardInfoBox/CardInfoBox.jsx
+++ src/ui/components/atoms/CardInfoBox/CardInfoBox.jsx
@@ -1,34 +1,34 @@
-import { CardSettingsBoxIcon } from '/src/assets/img/card/CardSettingsBoxIcon.jsx';
+import { CardInfoBoxIcon } from '/src/assets/img/card/CardInfoBoxIcon.jsx';
 
-import './CardSettingsBox.css';
+import './CardInfoBox.css';
 
-export const CardSettingsBox = ({ cardType, cardBank, cardName, cardNumber, cardStatus }) => {
+export const CardInfoBox = ({ cardType, cardBank, cardName, cardNumber, cardStatus }) => {
   return (
-    <div className={'card-setting-box'}>
-      <div className={`img-box ${cardBank}`}>
-        <CardSettingsBoxIcon className={cardBank}/>
+    <div className={'card-info-box'}>
+      <div className={`card-info-img ${cardBank}`}>
+        <CardInfoBoxIcon className={cardBank}/>
       </div>
-      <div className={'card-settings-content'}>
+      <div className={'card-info-content'}>
         <span>Bank</span>
         <span style={{background: 'transparent'}} className={cardBank}>{cardBank} Bank</span>
       </div>
-      <div className={'card-settings-content'}>
+      <div className={'card-info-content'}>
         <span>Card Type</span>
         <span style={{WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent'}} className={cardType}>{cardType}</span>
       </div>
-      <div className={'card-settings-content'}>
+      <div className={'card-info-content'}>
         <span>Card Number</span>
         <span>**** **** {cardNumber.slice(-4)}</span>
       </div>
-      <div className={'card-settings-content'}>
+      <div className={'card-info-content'}>
         <span>Card Name</span>
         <span className={'card-name'}>{cardName}</span>
       </div>
-      <div className={'card-settings-content'}>
+      <div className={'card-info-content'}>
         <span>Status</span>
         <span style={{color: `${cardStatus === 'Active' ? '#00c700' : 'red'}`}}>{cardStatus}</span>
       </div>
-      <div className={'button'}>
+      <div className={'card-info-button-box'}>
         <button>View Details</button>
       </div>
     </div>
