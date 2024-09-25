df2d7a6ca1f977f5a4b19b80c8110b6a7217da15
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-25 19:19:28 +0300

    update: cards content

--- src/ui/components/organisms/Cards/CardsContent.jsx
+++ src/ui/components/organisms/Cards/CardsContent.jsx
@@ -1,10 +1,20 @@
-import { CardExpenseStatsBox } from '/src/ui/components/molecules/CardExpenseStatsBox/CardExpenseStatsBox.jsx';
-
-import { NewCardBox } from '/src/ui/components/molecules/NewCardBox/NewCardBox.jsx';
 import { CardsListBox } from '/src/ui/components/molecules/CardsList/CardsList.jsx';
+import { CardExpenseStatsBox } from '/src/ui/components/molecules/CardExpenseStatsBox/CardExpenseStatsBox.jsx';
 import { CardsInfoList } from '/src/ui/components/molecules/CardsInfoList/CardsInfoList.jsx';
+import { NewCardBox } from '/src/ui/components/molecules/NewCardBox/NewCardBox.jsx';
+import { CardSettingsBox } from '/src/ui/components/molecules/CardSettingsBox/CardSettingsBox.jsx';
+
+import {useCards} from '/src/services/cards/hooks/useCards.js';
+import {auth} from '/src/services/api/firebase.js';
 
 export const CardsContent = () => {
+  const user = auth.currentUser;
+  const cards = useCards(user?.uid);
+
+  if (cards.length === 0) {
+    return <NewCardBox/>
+  }
+
   return (
     <div style={{display: 'flex', flexDirection: 'column', gap: 50}}> {/* TODO: delete div */}
       <CardsListBox/>
@@ -12,7 +22,10 @@ export const CardsContent = () => {
         <CardExpenseStatsBox/>
         <CardsInfoList/>
       </div>
-      <NewCardBox/>
+      <div style={{display: 'flex', gap: 30}}>
+        <NewCardBox/>
+        <CardSettingsBox/>
+      </div>
     </div>
   )
 }
