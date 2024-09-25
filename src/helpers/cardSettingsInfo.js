6f9530885a3d350da121d989cffeee546ffaae03
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-25 19:16:18 +0300

    feat: add card settings data

---
+++ src/helpers/cardSettingsInfo.js
@@ -0,0 +1,33 @@
+import blockCard from '/src/assets/img/cardSettings/block-card.svg';
+import activateCard from '/src/assets/img/cardSettings/activate-card.svg';
+import lockOpen from '/src/assets/img/cardSettings/lock-open.svg';
+import lockClose from '/src/assets/img/cardSettings/lock-close.svg';
+import cardTheme from '/src/assets/img/cardSettings/card-theme.svg';
+
+export const cardSettingsInfo = [
+  {
+    img: blockCard,
+    title: 'Block Card',
+    subtitle: 'Instantly block your card',
+  },
+  {
+    img: activateCard,
+    title: 'Activate Card',
+    subtitle: 'Activate your card',
+  },
+  {
+    img: lockOpen,
+    title: 'Add Pin Code',
+    subtitle: 'Select a pin code',
+  },
+  {
+    img: lockClose,
+    title: 'Change Pin Code',
+    subtitle: 'Choose another pin code',
+  },
+  {
+    img: cardTheme,
+    title: 'Card Theme',
+    subtitle: 'Choose card theme',
+  },
+]
