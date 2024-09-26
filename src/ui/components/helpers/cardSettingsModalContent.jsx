708f7dec8b4bded8cdd2d2edfddf5f1a603fbd78
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-26 14:54:13 +0300

    feat: add card settings modal content

---
+++ src/ui/components/helpers/cardSettingsModalContent.jsx
@@ -0,0 +1,7 @@
+export const cardSettingsModalContent = {
+  'Block Card': <div>block</div>,
+  'Activate Card': <div>activate</div>,
+  'Add Pin Code': <div>add pin</div>,
+  'Change Pin Code': <div>change pin</div>,
+  'Card Theme': <div>theme</div>,
+}
