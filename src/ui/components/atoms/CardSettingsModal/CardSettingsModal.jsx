ca4b6419e610aa77ad4c332202c10e44599fe7d4
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-27 14:46:05 +0300

    feat: add card settings modal title

--- src/ui/components/atoms/CardSettingsModal/CardSettingsModal.jsx
+++ src/ui/components/atoms/CardSettingsModal/CardSettingsModal.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './CardSettingsModal.css';
 
-export const CardSettingsModal = ({ isOpen, onClose, children }) => {
+export const CardSettingsModal = ({ isOpen, onClose, title, children }) => {
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add('no-scroll');
@@ -20,6 +20,7 @@ export const CardSettingsModal = ({ isOpen, onClose, children }) => {
     <div className={'modal-overlay'}>
       <div className={'modal-content'}>
         <button className={'modal-close-btn'} onClick={onClose}>✕</button>
+        <p className={'modal-title'}>{title}</p>
         {children}
       </div>
     </div>
