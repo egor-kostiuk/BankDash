f83f7f98725ea0928a5158a191d8ce33bafde118
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-27 14:51:09 +0300

    feat: add modal card box

---
+++ src/ui/components/molecules/ModalCardBox/ModalCardBox.jsx
@@ -0,0 +1,19 @@
+import { CurrencyText } from '/src/utils/CurrencyText.jsx';
+import { ModalCard } from '/src/ui/components/atoms/ModalCard/ModalCard.jsx';
+
+import './ModalCardBox.css';
+
+export const ModalCardBox = ({ cardBank, cardType, cardBalance, cardNumber }) => {
+  return (
+    <div className={'modal-card-box'}>
+      <div className={'modal-card-title'}>
+        <ModalCard cardBank={cardBank}/>
+        <span className={'modal-card-number'}>{`** ${cardNumber.slice(-4)}`}</span>
+      </div>
+      <span style={{WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent'}} className={`modal-card-type ${cardType}`}>{cardType}</span>
+      <span className={'modal-card-balance'}>
+        <CurrencyText>{cardBalance}</CurrencyText>
+      </span>
+    </div>
+  )
+}
